f0047b2c3de253a3542a32c6ba3a51fe30da24ab
rodrigomaganha <rodrigomaganha@fetch.ly>
2023-10-02 18:37:02 -0300

    feat(signin): added message errors

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -3,12 +3,20 @@ import { SignInStyle } from "./style";
 import { Login, signin } from "../../services/user";
 
 function SignIn() {
-	const { register, handleSubmit } = useForm<Login>();
+	const { register, handleSubmit, formState, setError } = useForm<Login>();
 
 	const submit: SubmitHandler<Login> = (user: Login) => {
 		signin(user).then((res) => {
 			console.log({res})
-		});
+		})
+		.catch((err) => {
+			const { response } = err;
+			const { data }: { data: { success: boolean, errors: string[] } } = response;
+
+			data.errors.forEach((error) => {
+				setError('email', { message: error });
+			});
+		})
 	};
 
 	return (
@@ -18,12 +26,14 @@ function SignIn() {
 			<form onSubmit={handleSubmit(submit)}>
 				<label>
 					Email
-					<input {...register('email')} type="email" placeholder="email" />
+					<input {...register('email', { required: 'email cannot be empty' })} type="email" placeholder="email" />
+					{ formState.errors?.email && <span>{formState.errors.email.message}</span> }
 				</label>
 
 				<label>
 					Password
-					<input {...register('password')} type="password" placeholder="your-password-here" />
+					<input {...register('password', { required: 'password cannot be empty' })} type="password" placeholder="your-password-here" />
+					{ formState.errors?.password && <span>{formState.errors.password.message}</span> }
 				</label>
 
 				<button type="submit"> signin </button>
