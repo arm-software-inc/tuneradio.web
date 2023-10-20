35a4e7cf08858bead7eea3dd66d047cc67c83183
abjesus <abel.jesus.silva@outlook.com>
2023-10-20 11:49:37 -0300

    fix: removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 // TODO: add google clientId to env file
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="554341061191-aqo36nc4r39c1kc82o92pu3qokebb5tm.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <App/>
   </GoogleOAuthProvider>
 )
